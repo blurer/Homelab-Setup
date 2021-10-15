53c48a696c328c31ebe7519b0ba282cdfbb3a268
Bryan Lurer <bl@bl-macbook.lan>
2021-10-15 21:37:44 +0900

    3

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ mkdir $HOME/docker
 mkdir $HOME/docker/proxy/
 mkdir $HOME/docker/dns/
 mkdir $HOME/docker/uptime-kuma/
-#mkdir $HOME/docker/portainer/
-#mkdir $HOME/docker/
+mkdir $HOME/docker/portainer/
+mkdir $HOME/docker/flame/
 
 echo "[Start Proxy]"
 cp files/proxy.yml $HOME/docker/proxy/docker-compose.yml
@@ -17,11 +17,16 @@ cp files/uptime-kuma.yml $HOME/docker/uptime-kuma/docker-compose.yml
 docker-compose -f $HOME/docker/uptime-kuma/docker-compose.yml up -d
 
 echo "[Portainer]"
-
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer    --restart=always    -v /var/run/docker.sock:/var/run/docker.sock    -v $HOME/docker/portainer/:/data  portainer/portainer-ce:latest
 
 echo "[DNS]"
 cp files/dns.yml $HOME/docker/dns/docker-compose.yml
 docker-compose -f $HOME/docker/dns/docker-compose.yml up -d
-
 sudo systemctl disable systemd-resolved.service
 sudo systemctl stop systemd-resolved
+docker start pihole
+
+echo "[Flame]"
+cp files/flame.yml $HOME/docker/flame/docker-compose.yml
+docker-compose -f $HOME/docker/flame/docker-compose.yml up -d
+
