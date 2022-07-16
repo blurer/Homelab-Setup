03a2322ec4f36f266f68f7a6c3bae631708bd11f
bl <blurer@pm.me>
2022-07-16 02:13:30 -0400

    fixed pihole for ubuntu

--- scripts/installDNS.sh
+++ scripts/installDNS.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+sudo systemctl restart systemd-resolved
+
 mkdir $HOME/docker/dns/
 cp files/dns.yml $HOME/docker/dns/docker-compose.yml
 docker-compose -f $HOME/docker/dns/docker-compose.yml up -d
